1e1078a32df14f9b7a8380d1f66a1d1a76fe1991
johnboeira <johnboeira@gmail.com>
2025-01-06 18:17:28 -0300

    feat: Cria um web service simples

---
+++ webservice/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe("localhost:3000", nil)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	f, _ := os.Open("./menu.txt")
+	io.Copy(w, f)
+
+}
