24332471f63734dd26cd8ae517aa0f8d50e79c58
johnboeira <johnboeira@gmail.com>
2025-01-14 14:46:55 -0300

    feat: Adiciona switch part 1

---
+++ switch/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	type menuItem struct {
+		name   string
+		prices map[string]float64
+	}
+
+	menu := []menuItem{
+		{name: "Coffee", prices: map[string]float64{"small": 1.65, "medium": 1.80, "large": 1.95}},
+		{name: "Espresso", prices: map[string]float64{"single": 1.90, "double": 2.25, "triple": 2.55}},
+	}
+
+	in := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Selecione uma opção")
+	fmt.Println("1) Menu")
+	fmt.Println("2) Add")
+	fmt.Println("q) Quit")
+	choice, _ := in.ReadString('\n')
+
+	switch strings.TrimSpace(choice) {
+	case "1":
+		for _, item := range menu {
+			fmt.Println(item.name)
+			fmt.Println(strings.Repeat("-", 10))
+			for size, price := range item.prices {
+				fmt.Printf("\t%10s%10.2f\n", size, price)
+			}
+		}
+	case "2":
+		fmt.Println("Digite o item novo:")
+		name, _ := in.ReadString('\n')
+		menu = append(menu, menuItem{name: name, prices: make(map[string]float64)})
+	}
+
+}
