f94342d9870d74fa20e59ee129c65ac7820fea5d
johnboeira <johnboeira@gmail.com>
2025-01-15 14:54:03 -0300

    feat: Quebrando um loop com label

--- switch/main.go
+++ switch/main.go
@@ -20,25 +20,33 @@ func main() {
 
 	in := bufio.NewReader(os.Stdin)
 
-	fmt.Println("Selecione uma opção")
-	fmt.Println("1) Menu")
-	fmt.Println("2) Add")
-	fmt.Println("q) Quit")
-	choice, _ := in.ReadString('\n')
+loop:
+	for {
+		fmt.Println("Selecione uma opção")
+		fmt.Println("1) Menu")
+		fmt.Println("2) Add")
+		fmt.Println("q) Quit")
+		choice, _ := in.ReadString('\n')
 
-	switch strings.TrimSpace(choice) {
-	case "1":
-		for _, item := range menu {
-			fmt.Println(item.name)
-			fmt.Println(strings.Repeat("-", 10))
-			for size, price := range item.prices {
-				fmt.Printf("\t%10s%10.2f\n", size, price)
+		switch strings.TrimSpace(choice) {
+		case "1":
+			for _, item := range menu {
+				fmt.Println(item.name)
+				fmt.Println(strings.Repeat("-", 10))
+				for size, price := range item.prices {
+					fmt.Printf("\t%10s%10.2f\n", size, price)
+				}
 			}
+		case "2":
+			fmt.Println("Digite o item novo:")
+			name, _ := in.ReadString('\n')
+			menu = append(menu, menuItem{name: name, prices: make(map[string]float64)})
+		case "q":
+			break loop
+		default:
+			fmt.Println("Opção não conhecida")
 		}
-	case "2":
-		fmt.Println("Digite o item novo:")
-		name, _ := in.ReadString('\n')
-		menu = append(menu, menuItem{name: name, prices: make(map[string]float64)})
+
 	}
 
 }
